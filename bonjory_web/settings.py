a35c74222703c7081f243e52e3293d1d3bc01438
klzu <ktr01kizu@gmail.com>
2025-11-14 21:53:19 +0900

    settingsのメール関係のミスを修正

--- bonjory_web/settings.py
+++ bonjory_web/settings.py
@@ -162,6 +162,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('MAIL_HOST_USER')  # 送信元となるメールアドレス
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # 送信元となるメールアドレス
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')        # アプリパスワードなど
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL') # 送信元として表示されるメールアドレス
